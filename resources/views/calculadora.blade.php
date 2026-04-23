572abcecce78bbebfaf54f9c3162aa282b3da638
Yasmin <yasmiinsantos20170@gmail.com>
2026-04-23 11:30:36 -0300

    Update calculadora.blade.php

--- resources/views/calculadora.blade.php
+++ resources/views/calculadora.blade.php
@@ -1,3 +1,22 @@
+<head>
+    <style>
+        body{
+            font-family:Arial, sans-serif;
+            background: #f2f2f2;
+            text-align:center;
+            margin-top:50px;
+        }
+        form{
+            background:#4e3391;
+            padding:20px;
+            border-radius:10px;
+            display: inline-block;
+            box-shadow:#1d063b;
+        }
+        
+    </style>
+</head>
+
 <form action="/calcular" method="POST">
     @csrf <input type="number" name="num1" placeholder="Número 1" required>
     
