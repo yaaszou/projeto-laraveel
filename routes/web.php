a10387f79328e83c16f94411750efe8940b1a50a
Yasmin <yasmiinsantos20170@gmail.com>
2026-04-23 11:23:22 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\CalculadoraController;
 use Illuminate\Support\Facades\Route;
 
-// Agora a página inicial (/) chama o seu Controller, não a 'welcome'
+
 Route::get('/', [CalculadoraController::class, 'index']);
 Route::post('/calcular', [CalculadoraController::class, 'calcular']);
